9a4bbf61644ada216702b53cbf8cfec3f1a2c207
Guilherme Sá <gsmangueira@gmail.com>
2023-05-23 16:40:25 -0300

    :warning: Add better error handling and messages
    Signed-off-by: Guilherme Sá <gsmangueira@gmail.com>

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -2,33 +2,77 @@ import propertyRepo from '../repositories/properties.js'
 import Property from '../models/property.js'
 
 export const listProperties = async (request, response) => {
-  const list = await propertyRepo.getProperties()
-  return response.status(200).json(list)
+  try {
+    const list = await propertyRepo.getProperties()
+    return response.status(200).json(list)
+  } catch (error) {
+    return response.status(500).json({ error: error.message })
+  }
 }
 
 export const getPropertyById = async (request, response) => {
-  const id = parseInt(request.params.id)
-  const result = await propertyRepo.getPropertyById(id)
-  return response.status(200).json(result)
+  try {
+    const id = parseInt(request.params.id)
+    if (isNaN(id)) {
+      return response.status(400).json({ error: 'Invalid ID' })
+    }
+    const result = await propertyRepo.getPropertyById(id)
+    if (result.length === 0) {
+      return response.status(404).json({ error: 'Property not found' })
+    }
+    return response.status(200).json(result)
+  } catch (error) {
+    return response.status(500).json({ error: error.message })
+  }
 }
 
 export const createProperty = async (request, response) => {
-  const { owner, address, city, state, zip_code} = request.body;
-  const property = new Property(owner, address, city, state, zip_code)
-  const result = await propertyRepo.createProperty(property)
-  return response.status(201).send(`Property added with ID: ${result.id}`)
+  try {
+    const { owner, address, city, state, zip_code } = request.body;
+    if (!owner || !address || !city || !state || !zip_code) {
+      return response.status(400).json({ error: 'Missing parameters' })
+    }
+    const property = new Property(owner, address, city, state, zip_code)
+    const result = await propertyRepo.createProperty(property)
+    return response.status(201).send(`Property added with ID: ${result.id}`)
+  } catch (error) {
+    return response.status(500).json({ error: error.message })
+  }
 }
 
 export const updateProperty = async (request, response) => {
-  const id = parseInt(request.params.id)
-  const { owner, address, city, state, zip_code} = request.body;
-  const property = new Property(owner, address, city, state, zip_code)
-  const result = await propertyRepo.updateProperty(id, property)
-  return response.status(200).send(`Property modified with ID: ${result.id}`)
+  try {
+    const id = parseInt(request.params.id)
+    if (isNaN(id)) {
+      return response.status(400).json({ error: 'Invalid ID' })
+    }
+    const { owner, address, city, state, zip_code } = request.body;
+    if (!owner || !address || !city || !state || !zip_code) {
+      return response.status(400).json({ error: 'Missing parameters' })
+    }
+    const property = new Property(owner, address, city, state, zip_code)
+    const result = await propertyRepo.updateProperty(id, property)
+    if (result.length === 0) {
+      return response.status(404).json({ error: 'Property not found' })
+    }
+    return response.status(200).send(`Property modified with ID: ${result.id}`)
+  } catch (error) {
+    return response.status(500).json({ error: error.message })
+  }
 }
 
 export const deleteProperty = async (request, response) => {
-  const id = parseInt(request.params.id)
-  const result = await propertyRepo.deleteProperty(id, response)
-  return response.status(200).send(`Property deleted with ID: ${result.id}`)
+  try {
+    const id = parseInt(request.params.id)
+    if (isNaN(id)) {
+      return response.status(400).json({ error: 'Invalid ID' })
+    }
+    const result = await propertyRepo.deleteProperty(id)
+    if (result.length === 0) {
+      return response.status(404).json({ error: 'Property not found' })
+    }
+    return response.status(200).send(`Property deleted with ID: ${result.id}`)
+  } catch (error) {
+    return response.status(500).json({ error: error.message })
+  }
 }
--- repositories/properties.js
+++ repositories/properties.js
@@ -11,42 +11,63 @@ const client = new Client({
   port: process.env.DB_PORT,
 })
 client.connect()
-    .catch((err) => console.error('connection error', err.stack))
-
+  .catch((err) => {
+    throw new Error('Connection error: ' + err.stack);
+  });
 
 const getProperties = async () => {
-  let response = await client.query('SELECT * FROM properties ORDER BY id ASC')
-  return response.rows;
+  try {
+    let response = await client.query('SELECT * FROM properties ORDER BY id ASC');
+    return response.rows;
+  } catch (error) {
+    throw new Error('Failed to get properties: ' + error.message);
+  }
 }
 
-const getPropertyById = async (id) => {  
-  let response = await client.query('SELECT * FROM properties WHERE id = $1', [id])
-  return response.rows;
+const getPropertyById = async (id) => {
+  try {
+    let response = await client.query('SELECT * FROM properties WHERE id = $1', [id]);
+    return response.rows;
+  } catch (error) {
+    throw new Error('Failed to get property by ID: ' + error.message);
+  }
 }
 
 const createProperty = async (property) => {
-  const { owner, address, city, state, zip_code} = property;
-  let response =  await client.query('INSERT INTO properties (owner, address, city, state, zip_code) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-    [owner, address, city, state, zip_code])
-  return response.rows[0];
+  try {
+    const { owner, address, city, state, zip_code } = property;
+    let response = await client.query('INSERT INTO properties (owner, address, city, state, zip_code) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [owner, address, city, state, zip_code]);
+    return response.rows[0];
+  } catch (error) {
+    throw new Error('Failed to create property: ' + error.message);
+  }
 }
 
 const updateProperty = async (id, property) => {
-  const { owner, address, city, state, zip_code} = property
-  let response = await client.query('UPDATE properties SET owner = $1, address = $2, city = $3, state = $4, zip_code = $5 WHERE id = $6 RETURNING *',
-    [owner, address, city, state, zip_code, id]);
-  return response.rows[0];
+  try {
+    const { owner, address, city, state, zip_code } = property;
+    let response = await client.query('UPDATE properties SET owner = $1, address = $2, city = $3, state = $4, zip_code = $5 WHERE id = $6 RETURNING *',
+      [owner, address, city, state, zip_code, id]);
+    return response.rows[0];
+  } catch (error) {
+    throw new Error('Failed to update property: ' + error.message);
+  }
 }
 
 const deleteProperty = async (id) => {
-  let response = await client.query('DELETE FROM properties WHERE id = $1 RETURNING *', [id])  
-  return response.rows[0];
+  try {
+    let response = await client.query('DELETE FROM properties WHERE id = $1 RETURNING *', [id]);
+    return response.rows[0];
+  } catch (error) {
+    throw new Error('Failed to delete property: ' + error.message);
+  }
 }
 
 export default {
-    getProperties,
-    getPropertyById,
-    createProperty,
-    updateProperty,
-    deleteProperty,
-}
+  getProperties,
+  getPropertyById,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+};
